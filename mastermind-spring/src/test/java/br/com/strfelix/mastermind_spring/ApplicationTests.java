c0dd46700391d4856c12a09359838b4211899277
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-03-16 23:41:50 -0300

    initial commit

---
+++ mastermind-spring/src/test/java/br/com/strfelix/mastermind_spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.strfelix.mastermind_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
