e4201541659aa2971c5fab67cbf1326638fbd4d6
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-03-23 23:12:17 -0300

    fix: handle all localhost origns and options perflight

--- mastermind-spring/src/main/java/br/com/strfelix/mastermind_spring/config/security/CorsConfig.java
+++ mastermind-spring/src/main/java/br/com/strfelix/mastermind_spring/config/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:*")
+                        .allowedOriginPatterns("http://localhost:*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- mastermind-spring/src/main/java/br/com/strfelix/mastermind_spring/config/security/SecurityConfig.java
+++ mastermind-spring/src/main/java/br/com/strfelix/mastermind_spring/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -58,7 +59,8 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                            .requestMatchers("/auth/**").permitAll();
                     for (String route : extraPermits) {
                         auth.requestMatchers(route).permitAll();
                     }
